68142c54f6310f9b7a5f37f743f52e9c57e1f58d
sohyun-99 <61389446+sohyun-99@users.noreply.github.com>
2023-07-27 13:39:40 +0900

    Create 입국심사.py

---
+++ 5주차_정렬,이진탐색/PG_입국심사/입국심사.py
@@ -0,0 +1,20 @@
+def solution(n, times):
+    answer = 0
+    start, end = 1,max(times)*n # 최대 시간 값
+
+
+    while(start<=end):
+        mid = (start+end)//2
+        people=0
+        for t in times:
+            people+=mid//t # mid일때 수행할 수 있는 사람수 구하기
+            if people >= n:
+                break
+            
+        if people < n:
+            start=mid+1
+        elif people >= n :
+            answer = mid
+            end = mid -1
+
+    return answer
